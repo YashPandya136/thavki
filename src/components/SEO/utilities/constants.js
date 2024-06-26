de220e724f79122a4dad6b3e06c278accfeb21ba
Yash <70088826+YashPandya136@users.noreply.github.com>
2024-06-26 17:47:31 +0530

    Update constants

--- src/components/SEO/utilities/constants.js
+++ src/components/SEO/utilities/constants.js
@@ -77,20 +77,20 @@ export const SOCIAL_BASE_LINKS = {
 
 
 export const HOME_SEO_DATA = {
-  title: 'Bkthavki | Surat',
+  title: 'B. K. Silk Mils Pvt. ltd | Surat',
   description: 'Discover the finest silk fabrics at B. K. Silk Mills Pvt. Ltd. (bkthavki), a leading textile manufacturer based in Surat, India. Renowned for our exceptional quality and exquisite designs, we specialize in producing a wide range of silk materials for the fashion and home decor industries. Our commitment to innovation and craftsmanship ensures that every piece meets the highest standards. Explore our extensive collection and experience the elegance and luxury of B. K. Silk Mills Pvt. Ltd(bkthavki).',
   keywords: 'bkthavki, Silk fabrics, Textile manufacturer Surat, Silk mills Surat, B. K. Silk Mills(bkthavki), Silk material supplier, Luxury silk fabrics, Surat silk industry, Silk fabric production, High-quality silk textiles, Silk fabric exporter, Best silk fabric manufacturer in Surat, Leading silk textile producers in India, Wholesale silk fabric suppliers, Custom silk fabric designs, Premium silk materials for fashion and home decor, Silk fabric wholesale Surat, Silk production company, Silk fabric distributor, Surat silk market, Indian silk textiles'
 }
 
 
 export const CONTACT_SEO_DATA = {
-  title: 'Contact Us | bkthavki',
+  title: 'Contact Us | B. K. Silk Mils Pvt. ltd',
   description: 'Get in touch with Thavki Sarees for inquiries about our exquisite collection of silk sarees, including Kanjivaram, Dharmavaram, Kattu, and pure silk varieties. Our team is here to assist you with finding the perfect saree for any occasion. Contact us today to learn more about our products and services.',
   keywords: 'Contact Thavki Sarees, Silk saree inquiries, Kanjivaram silk sarees contact, Dharmavaram silk sarees contact, Kattu silk sarees contact, Pure silk sarees contact, Designer silk sarees contact, High-quality silk sarees contact, Affordable silk sarees contact, Silk saree customer service, Wedding saree inquiries, Anniversary saree inquiries, Festival saree inquiries, Party saree inquiries'
 }
 
 export const ABOUT_SEO_DATA = {
-  title: 'About Us | bkthavki',
+  title: 'About Us | B. K. Silk Mils Pvt. ltd',
   description: "Discover the elegance of Thavki Sarees, where we specialize in high-quality silk sarees crafted with superior yarns. Our collection features Kanjivaram, Dharmavaram, Kattu, and pure silk sarees, perfect for weddings, anniversaries, festivals, and parties. With advanced technology and industry collaborations, we offer the latest fashion trends at affordable prices. Explore our designer sarees today!",
   keywords: 'Thavki Sarees, Silk sarees, Kanjivaram silk sarees, Dharmavaram silk sarees, Kattu silk sarees, Pure silk sarees, Designer silk sarees, High-quality silk sarees, Affordable silk sarees, Latest silk saree trends, Wedding sarees, Anniversary sarees, Festival sarees,Party sarees'
 }
